867e18401c55e0a694839b25928f5ef3a3575548
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-01-24 16:03:57 +0100

    register page

---
+++ src/register.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const register = () => {
+  return (
+    <View>
+      <Text>register</Text>
+    </View>
+  )
+}
+
+export default register
