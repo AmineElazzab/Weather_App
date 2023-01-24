370384c5ceddedaa17fc5ad87abf4dc001af6179
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-01-24 16:03:13 +0100

    next 7 days

---
+++ src/days.jsx
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const days
+ = () => {
+  return (
+    <View>
+      <Text>days
+        
+      </Text>
+    </View>
+  )
+}
+
+export default days
