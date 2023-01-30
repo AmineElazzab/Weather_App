c139309b9a34474fdab21591ac698ca9af349c6e
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-01-30 11:51:02 +0100

    update

--- App.js
+++ App.js
@@ -1,18 +1,25 @@
 // import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import Weather from './src/home';
-// import Days from './src/days';
+import Days from './src/days';
+import Login from './src/login';
+import Register from './src/register';
 
 
 
 
 
 export default function App() {
+
   return (
     <View style={styles.container}>
       <Weather />
       {/* <Days /> */}
+      {/* <Login /> */}
+      {/* <Register /> */}
     </View>
+
+    
   );
 }
 
