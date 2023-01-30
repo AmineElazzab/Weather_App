8d836c824d0881c20ddd36fe5959c6358d169a00
mohammed amine elazzab <ma.elazzab@gmail.com>
2023-01-30 11:51:33 +0100

    add background images

---
+++ assets/background/index.js
@@ -0,0 +1,30 @@
+const haze = require('./img/haze.jpg');
+const rainy = require('./img/rainy.jpg');
+const sunny = require('./img/clear.jpg');
+const snowy = require('./img/snow.jpg');
+const cloudy = require('./img/cloudy.jpg');
+const mist = require('./img/mist.jpg')
+const thunderstorm = require('./img/lightning-cozy.gif')
+const drizzle = require('./img/drizzle.jpg')
+const fog = require('./img/Fuel_Forest.gif')
+const smoke = require('./img/smoke.jpg')
+const dust = require('./img/dust.jpg')
+const sand = require('./img/sand.jpg')
+const tornado = require('./img/tornado.jpg')
+
+
+export {
+    haze,
+    rainy,
+    sunny,
+    cloudy,
+    snowy,
+    mist,
+    thunderstorm,
+    drizzle,
+    fog,
+    smoke,
+    dust,
+    sand,
+    tornado
+};
